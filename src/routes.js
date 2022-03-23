8e986d35fd806b65e2fdbf587746762a9d1b4322
moises.neto <moisesnetored@gmail.com>
2022-03-23 01:10:27 -0400

    feat: Typescript added, use of envs added.

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express');
-const SendMessageController = require('./controller/sendMessageController');
-
-const router = Router();
-
-router.post('/send', SendMessageController.sendMessage);
-
-module.exports = router;
-
